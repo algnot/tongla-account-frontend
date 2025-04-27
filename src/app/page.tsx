32d5a802d260d0b740344ca53e86c325336d5e19
algnot <thanawat.tongla@gmail.com>
2025-04-27 16:47:54 +0700

    fix up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client";
-import Image from "next/image";
 import { useHelperContext } from "@/components/providers/helper-provider";
 import { useEffect } from "react";
 
@@ -13,7 +12,5 @@ export default function Home() {
     }
   }, [userData]);
 
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
-  );
+  return <div></div>;
 }
