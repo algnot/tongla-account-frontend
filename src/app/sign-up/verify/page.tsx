ba0e0c15c7a56d7f1b70f606e72bfdabb7bb4d34
algnot <thanawat.tongla@gmail.com>
2025-04-28 15:07:51 +0700

    fix up

--- src/app/sign-up/verify/page.tsx
+++ src/app/sign-up/verify/page.tsx
@@ -1,11 +1,26 @@
 "use client";
 import { useHelperContext } from "@/components/providers/helper-provider";
 import { Button } from "@/components/ui/button";
+import { isErrorResponse } from "@/types/request";
 import React from "react";
 
 export default function VerifyPage() {
-  const { searchParams } = useHelperContext()();
+  const { searchParams, backendClient, setAlert, router, setFullLoading } = useHelperContext()();
   const email = searchParams.get("email");
+
+  const onResendVerificationEmail = async () => {
+    setFullLoading(true);
+    const response = await backendClient.login(email ?? "");
+    setFullLoading(false);
+
+    if (isErrorResponse(response)) {
+      return;
+    }
+
+    setAlert("error", "Your account is already verified try to login", () => {
+      router.push("/login");
+    }, false);
+  };
 
   return (
     <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
@@ -15,7 +30,7 @@ export default function VerifyPage() {
           Please check your <span className="text-red-500">{email}</span> for
           continue
         </p>
-        <Button>Resend verification email</Button>
+        <Button onClick={onResendVerificationEmail}>Resend verification email</Button>
       </div>
     </div>
   );
